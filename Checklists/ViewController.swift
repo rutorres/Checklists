0e0afc3d281345c4c2ac3bbe6e2c4e9afa47c3a5
Ruth Torres Castillo <rutorrres@nmsu.edu>
2018-02-21 14:15:52 -0700

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Ruth Torres Castillo on 2/21/18.
+//  Copyright © 2018 New Mexico State University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
