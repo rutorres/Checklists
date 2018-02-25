cd51580997169ac99ffcf5e06708869693936a2f
Ruth Torres Castillo <rutorrres@nmsu.edu>
2018-02-25 11:27:35 -0700

    End of Chapter 12

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,7 +25,34 @@ class AddItemViewController: UITableViewController {
     }
     
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView,
+            willSelectRowAt indexPath: IndexPath)
+            -> IndexPath? {
+        return nil
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        /*if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }*/
+        
+        doneBarButton.isEnabled = !(newText.isEmpty)
+        return true
     }
 
    
